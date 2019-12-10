fbd4b2fb635c554e95cacfb37c5feaa7163703e7
Narsibhai <12001001539311@FEDIDCARD.GOV>
2019-12-09 20:59:34 -0500

    Inetial Commit

---
+++ WebAPIDemo.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using WebAPIdemo.Controllers;
+
+namespace WebAPIDemo.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+        ValuesController _controller=new ValuesController();
+        [Fact]
+        public void GetReturnValues()
+        {
+        //Given
+        var returnvalue=_controller.Get(1);
+        Assert.Equal("value",returnvalue.Value);
+      
+        }
+
+    }
+}
